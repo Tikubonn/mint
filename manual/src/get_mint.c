b8728abd7780da4890c6a3c147218cefa33c308a
tikubonn <https://twitter.com/tikubonn>
2018-04-11 19:17:09 +0900

    a function of get_mint return a filled integer by 0 or 1 if it referenced the out of range.

--- manual/src/get_mint.c
+++ manual/src/get_mint.c
@@ -1,5 +1,6 @@
 #include <mint.h>
 #include "size_mint.h"
+#define is_negative(a) (a & 0b10000000)
 
 mint_cell get_mint (unsigned long index, unsigned int seek, mint *numin){
 	mint_cell numc = 0;
@@ -10,9 +11,15 @@ mint_cell get_mint (unsigned long index, unsigned int seek, mint *numin){
 		numc &= ~(0xff >> seeka);
 		numc |= numin->address_beginning[index] >> seeka;
 	}
+	else {
+		numc |= (is_negative(numin->address_beginning[size -1]) ? 0xff : 0) >> seeka;
+	}
 	if (index +1 < size){
 		numc &= ~(0xff << seekb);
 		numc |= numin->address_beginning[index +1] << seekb;
+	}
+	else {
+		numc |= (is_negative(numin->address_beginning[size -1]) ? 0xff : 0) << seekb;
 	}
 	return numc;
 }
