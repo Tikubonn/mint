6f0d39b93e8dacb8868101921d27cf2b41d8fd7d
tikubonn <https://twitter.com/tikubonn>
2018-04-16 11:33:14 +0900

    fixed bug in coping routine.

--- tmp/src/copy_tmp_mint.h
+++ tmp/src/copy_tmp_mint.h
@@ -4,4 +4,4 @@
 #define copy_tmp_mint(var, num)\
 mint *__ ## var ## _copy_argument = num;\
 make_tmp_mint(var, size_mint(__ ## var ## _copy_argument));\
-copy_mint_manually(var, __ ## var ## _copy_argument);
+copy_mint_manually(__ ## var ## _copy_argument, var);
--- tmp/src/floor_tmp_mint.h
+++ tmp/src/floor_tmp_mint.h
@@ -4,6 +4,6 @@
 #define floor_tmp_mint(var1, var2, numa, numb)\
 mint *__ ## var1 ## _floor_argument1 = numa;\
 mint *__ ## var1 ## _floor_argument2 = numb;\
-copy_tmp_mint(var1, __ ## var1 ## _floor_argument1);\
-make_tmp_mint(var2, size_mint(__ ## var1 ## _floor_argument1));\
-floor_mint_manually(var1, __ ## var1 ## _floor_argument2, var2);
+make_tmp_mint(var1, size_mint(__ ## var1 ## _floor_argument1));\
+copy_tmp_mint(var2, __ ## var1 ## _floor_argument1);\
+floor_mint_manually(var2, __ ## var1 ## _floor_argument2, var1);
