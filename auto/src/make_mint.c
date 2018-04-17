9b69d9ce60543b736aef89903511cb7b448e0e92
tikubonn <https://twitter.com/tikubonn>
2018-04-17 10:40:57 +0900

    a factory function of make_mint_from_string was added.

--- auto/src/make_mint.c
+++ auto/src/make_mint.c
@@ -20,3 +20,58 @@ mint *make_mint_from_long (long num){
   load_mint_from_long(num, numout);
   return numout;
 }
+
+static mint *make_mint_from_string_negative (char *source){
+  mint *num10 = make_mint_from_int(10);
+  mint *numcurrent = make_mint_from_int(0);
+  for (; *source; source++){
+    if (*source < '0' || '9' < *source){
+      free_mint(num10);
+      free_mint(numcurrent);
+      return NULL;
+    }
+    mint *nummul = mul_mint(numcurrent, num10);
+    mint *numchar = make_mint_from_int(*source - '0');
+    mint *numsub = sub_mint(nummul, numchar);
+    free_mint(numcurrent);
+    free_mint(nummul);
+    free_mint(numchar);
+    numcurrent = numsub;
+  }
+  return numcurrent;
+}
+
+static mint *make_mint_from_string_positive (char *source){
+  mint *num10 = make_mint_from_int(10);
+  mint *numcurrent = make_mint_from_int(0);
+  for (; *source; source++){
+    if (*source < '0' || '9' < *source){
+      free_mint(num10);
+      free_mint(numcurrent);
+      return NULL;
+    }
+    mint *nummul = mul_mint(numcurrent, num10);
+    mint *numchar = make_mint_from_int(*source - '0');
+    mint *numadd = add_mint(nummul, numchar);
+    free_mint(numcurrent);
+    free_mint(nummul);
+    free_mint(numchar);
+    numcurrent = numadd;
+  }
+  return numcurrent;
+}
+
+mint *make_mint_from_string (char *source){
+  if (*source == 0){
+    return NULL;
+  }
+  if (*source == '-'){
+    source++;
+    return make_mint_from_string_negative(source);
+  }
+  if (*source == '+'){
+    source++;
+    return make_mint_from_string_positive(source);
+  }
+  return make_mint_from_string_positive(source);
+}
--- auto/src/make_mint.h
+++ auto/src/make_mint.h
@@ -4,3 +4,4 @@
 extern mint *make_mint (unsigned long);
 extern mint *make_mint_from_int (int);
 extern mint *make_mint_from_long (long);
+extern mint *make_mint_from_string (char*);
