a3b5a7ca671d8cf7f72aba295feac49fb40a8302
tikubonn <https://twitter.com/tikubonn>
2018-04-17 11:41:44 +0900

    a property accessor of real_size_mint was added.

--- manual/src/size_mint.c
+++ manual/src/size_mint.c
@@ -3,3 +3,7 @@
 unsigned long size_mint (mint *numin){
   return (numin->address - numin->address_beginning) / sizeof(mint_cell);
 }
+
+unsigned long real_size_mint (mint *numin){
+  return (numin->address_end - numin->address_beginning) / sizeof(mint_cell);
+}
--- manual/src/size_mint.h
+++ manual/src/size_mint.h
@@ -1,3 +1,4 @@
 #include <mint.h>
 
 extern unsigned long size_mint (mint*);
+extern unsigned long real_size_mint (mint*);
