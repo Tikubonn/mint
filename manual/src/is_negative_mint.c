e1c56b87af2b9c490634b4ffa739094ccdb2ed8a
tikubonn <https://twitter.com/tikubonn>
2018-04-23 12:38:00 +0900

    fixed bug that rewrote size_mint to real_size_mint.

--- manual/src/is_negative_mint.c
+++ manual/src/is_negative_mint.c
@@ -2,6 +2,7 @@
 #define is_negative(a) (a & 0b10000000)
 
 int is_negative_mint (mint *num){
-  unsigned long size = size_mint(num);
+  // unsigned long size = size_mint(num);
+  unsigned long size = real_size_mint(num);
   return is_negative(num->address_beginning[size -1]) ? 1 : 0;
 }
--- manual/src/is_positive_mint.c
+++ manual/src/is_positive_mint.c
@@ -2,6 +2,7 @@
 #define is_positive(a) !(a & 0b10000000)
 
 int is_positive_mint (mint *num){
-  unsigned long size = size_mint(num);
+  // unsigned long size = size_mint(num);
+  unsigned long size = real_size_mint(num);
   return is_positive(num->address_beginning[size -1]);
 }
