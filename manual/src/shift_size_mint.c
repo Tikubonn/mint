a9879bcea98e9866651d17794711e0b54d5998e8
tikubonn <https://twitter.com/tikubonn>
2018-04-24 10:34:26 +0900

    fixed bug.

--- manual/src/shift_size_mint.c
+++ manual/src/shift_size_mint.c
@@ -1,21 +1,15 @@
 #include <mint.h>
 
 unsigned long seek_size_mint (mint *num){
-  unsigned long inum = 0;
-  unsigned long count = sizeof(long);
-  while (count--){
-    mint_cell numc = get_mint(count, 0, num);
-    inum <<= 8;
-    inum = inum | numc;
-  }
-  return inum;
+  unsigned long numc = get_mint(0, 0, num);
+  return numc & 7u;
 }
 
 unsigned long offset_size_mint (mint *num){
   unsigned long inum = 0;
-  unsigned long count = sizeof(long);
+  int count = sizeof(long);
   while (count--){
-    mint_cell numc = get_mint(count + sizeof(long), 0, num);
+    mint_cell numc = get_mint(count, 3u, num);
     inum <<= 8;
     inum = inum | numc;
   }
